d675b4c5cd909de7b02038b8dc0395a903afe54d
Alejandro <dalejo082715@gmail.com>
2024-02-17 15:41:09 -0500

    Aca esta la calculadora con 3 datos

--- test_git.py
+++ test_git.py
@@ -1,1 +1,6 @@
 print("Esta es una prueba de lrepositorio xd")
+
+#creo que voy a hacer un calculadora 
+dato1=float(input("Coloque un numero: "))
+dato2=float(input("Coloque un segundo numero: "))
+dato3=float(input ("Coloque un tercer numero"))
